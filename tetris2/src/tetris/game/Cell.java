c8840cdb37aa541784c3563866ad1a971ff72585
MaksimKosyhin <104004024+MaksimKosyhin@users.noreply.github.com>
2022-05-09 17:32:10 +0300

    Update Cell.java

--- tetris2/src/tetris/game/Cell.java
+++ tetris2/src/tetris/game/Cell.java
@@ -23,8 +23,4 @@ public class Cell extends Rectangle{
 		widthProperty().bind(size);
 		heightProperty().bind(size);
 	}
-	
-	public boolean isAtSamePosition(Cell c) {
-		return c.x.get() == x.get();
-	}
 }
